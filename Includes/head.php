5d21ff134336b15329fef190a5eb1f0d04068b38
Hamdi Anıl Hacıhaliloğlu <96963553+hamdihacihaliloglu@users.noreply.github.com>
2022-04-17 01:15:50 +0300

    Add files via upload

---
+++ Includes/head.php
@@ -0,0 +1,17 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="Content-Language" content="tr">
+    <meta charset="utf-8">
+    <meta name="robots" content="index, follow">
+    <meta name="googlebot" content="index, follow">
+    <meta name="revisit-after" content="7 Days">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="Hamdi Anıl Hacıhaliloğlu ">
+    <meta name="designer" content="Hamdi Anıl Hacıhaliloğlu ">
+   
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">  
+	<link href="Lib/Assets/style.css" rel="stylesheet"> 
+  	<script type="text/javascript" href="https://code.jquery.com/jquery-3.6.0.min.js" language="javascript"></script>
+</head>
---
+++ Includes/scripts.php
@@ -0,0 +1,4 @@
+<script src="https://kit.fontawesome.com/3ba88f0d82.js" crossorigin="anonymous"></script>	
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" ></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" ></script>
